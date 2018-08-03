7541abc2e93b588115e4edfb1ed00c1e5a680ccb
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-08-03 17:03:41 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const GITHUB_FETCH_URL = 'https://api.github.com/users/jakerjohnson94';
 const Card = props => (
   <div className="card">
     <div className="card-image">
-      <figure className="image is-3by3">
+      <figure className="image is-4">
         <img src={props.avatar_url} alt="Github Profile Image" />
       </figure>
     </div>
@@ -16,11 +16,12 @@ const Card = props => (
           <p className="subtitle is-6">{props.location}</p>
         </div>
       </div>
-    </div>
-    <div className="content">
-      {props.bio}
-      <br />
-      {props.public_repos} Public Repositories
+
+      <div className="content">
+        <p>{props.bio}</p>
+
+        <a href="https://github.com/jakerjohnson94">{props.public_repos} Public Repositories</a>
+      </div>
     </div>
   </div>
 );
@@ -34,29 +35,33 @@ class App extends Component {
       .then(results => results.json())
       .then(data => {
         this.setState({ user: data });
-        this.state.active === false
-          ? this.setState({ active: true })
-          : this.setState({ active: false });
+        this.state.active === true
+          ? this.setState({ active: false })
+          : this.setState({ active: true });
       });
 
   render() {
     const { user = {} } = this.state;
     const isActive = this.state.active;
     return (
-      <div>
-        <button id="cardBtn" className="button" onClick={this.handleClick}>
-          Toggle Card
-        </button>
-        {isActive ? (
-          <Card
-            avatar_url={user.avatar_url}
-            name={user.name}
-            location={user.location}
-            bio={user.bio}
-            public_repos={user.public_repos}
-          />
-        ) : null}
-      </div>
+      <React.Fragment>
+        <div className="buttonWrapper">
+          <button id="cardBtn" className="button" onClick={this.handleClick}>
+            Toggle Card
+          </button>
+        </div>
+        <div className="cardWrapper">
+          {isActive ? (
+            <Card
+              avatar_url={user.avatar_url}
+              name={user.name}
+              location={user.location}
+              bio={user.bio}
+              public_repos={user.public_repos}
+            />
+          ) : null}
+        </div>
+      </React.Fragment>
     );
   }
 }
